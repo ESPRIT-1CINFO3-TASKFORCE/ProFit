ae207063c2a7e4142a2abd02efcce3fb5802cef2
trabelsibahe <baha.trabelsi@gmail.com>
2024-06-03 12:48:10 +0100

    Ajout des dépendances JAVAFX. Création d'une nouvelle interface graphique : Chat. Mise à jour sur DataSource. MessageEntity. MessageService.

--- src/main/java/TestMain/main.java
+++ src/main/java/TestMain/main.java
@@ -1,4 +1,20 @@
 package TestMain;
 
+import Entites.MessageEntity;
+import Services.MessageService;
+
+import java.time.LocalDateTime;
+
 public class main {
+    public static void main(String[] args) {
+        MessageService messageService = new MessageService();
+
+        MessageEntity message = new MessageEntity();
+        message.setEnvoyeur(1);
+        message.setRecepteur(2);
+        message.setMessage("Hello, this is a test message!");
+        message.setTimestamp(LocalDateTime.now());
+
+        //FOR TEST messageService.insertMessage(message);
+    }
 }
--- src/main/java/Utils/DataSource.java
+++ src/main/java/Utils/DataSource.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataSource {
-    private  String url="jdbc:mysql://localhost:3306/pidev";
+    private  String url="jdbc:mysql://localhost:3306/profit_bd";
     private  String login="root";
     private  String pwd="";
     private static DataSource data;
@@ -16,10 +16,10 @@ public class DataSource {
     private DataSource(){
         try {
             con= DriverManager.getConnection(url,login,pwd);
-            System.out.println("connexion établie");
+            System.out.println("Connexion établie avec la base de données profit_bd. ");
         } catch (SQLException e) {
 
-            System.out.println(e);
+            System.out.println("Impossible de se connecter à la base de données : "+e);
         }
     }
     public Connection getCon() {
